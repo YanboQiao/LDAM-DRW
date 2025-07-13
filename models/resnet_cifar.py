66fae58f7a00034d129373625aa79a27eade97af
yanbo Joe <qiaoyanbo@kuaishou.com>
2025-07-13 16:37:56 +0800

    checkpoint

--- models/resnet_cifar.py
+++ models/resnet_cifar.py
@@ -88,41 +88,38 @@ class BasicBlock(nn.Module):
 
 
 class ResNet_s(nn.Module):
-
     def __init__(self, block, num_blocks, num_classes=10, use_norm=False):
-        super(ResNet_s, self).__init__()
+        super().__init__()
         self.in_planes = 16
-
-        self.conv1 = nn.Conv2d(3, 16, kernel_size=3, stride=1, padding=1, bias=False)
+        self.conv1 = nn.Conv2d(3, 16, 3, 1, 1, bias=False)
         self.bn1 = nn.BatchNorm2d(16)
-        self.layer1 = self._make_layer(block, 16, num_blocks[0], stride=1)
-        self.layer2 = self._make_layer(block, 32, num_blocks[1], stride=2)
-        self.layer3 = self._make_layer(block, 64, num_blocks[2], stride=2)
-        if use_norm:
-            self.linear = NormedLinear(64, num_classes)
-        else:
-            self.linear = nn.Linear(64, num_classes)
+        self.layer1 = self._make_layer(block, 16, num_blocks[0], 1)
+        self.layer2 = self._make_layer(block, 32, num_blocks[1], 2)
+        self.layer3 = self._make_layer(block, 64, num_blocks[2], 2)
+        self.use_norm = use_norm
+        self.linear = NormedLinear(64, num_classes) if use_norm else nn.Linear(64, num_classes)
         self.apply(_weights_init)
 
     def _make_layer(self, block, planes, num_blocks, stride):
         strides = [stride] + [1]*(num_blocks-1)
         layers = []
-        for stride in strides:
-            layers.append(block(self.in_planes, planes, stride))
+        for s in strides:
+            layers.append(block(self.in_planes, planes, s))
             self.in_planes = planes * block.expansion
-
         return nn.Sequential(*layers)
 
-    def forward(self, x):
+    # 新增 return_feat 参数
+    def forward(self, x, return_feat=False):
         out = F.relu(self.bn1(self.conv1(x)))
         out = self.layer1(out)
         out = self.layer2(out)
         out = self.layer3(out)
-        out = F.avg_pool2d(out, out.size()[3])
-        out = out.view(out.size(0), -1)
-        out = self.linear(out)
-        return out
-
+        out = F.avg_pool2d(out, out.size(3))
+        feat = out.view(out.size(0), -1)
+        logits = self.linear(feat)
+        if return_feat:
+            return logits, feat
+        return logits
 
 def resnet20():
     return ResNet_s(BasicBlock, [3, 3, 3])
